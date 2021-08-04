53ba05838b9f3d0915eb5dc8afdee5f60300ea99
Ivan Limarenko <limarenko.vanya@yandex.ru>
2021-08-05 01:50:34 +0300

    webpack configuration

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styles from './header.scss';
+
+console.log(styles);
+
+export function Header() {
+  return(
+    <header>
+      <h1 className={styles.title}><span className={styles.span}>Hello</span> React</h1>
+    </header>
+  );
+}
