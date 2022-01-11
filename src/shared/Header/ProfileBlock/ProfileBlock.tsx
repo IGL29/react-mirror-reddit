3e7dab483b68b75b8e7559ed8e46c7a3a3fc34c6
Ivan Limarenko <i.limarenko@brightlab.me>
2022-01-11 13:23:16 +0300

    fix_link

--- src/shared/Header/ProfileBlock/ProfileBlock.tsx
+++ src/shared/Header/ProfileBlock/ProfileBlock.tsx
@@ -9,15 +9,28 @@ interface IProfileBlockProps {
   avatarSrc?: string;
 }
 
-const ID = process.env.CLIENT_ID
+const ID = process.env.CLIENT_ID;
 
-export default function ProfileBlock({className, userName, avatarSrc}: IProfileBlockProps): JSX.Element {
+export default function ProfileBlock({
+  className,
+  userName,
+  avatarSrc,
+}: IProfileBlockProps): JSX.Element {
   return (
-    <a href={`https://www.reddit.com/api/v1/authorize?client_id=${ID}&response_type=code&
-    state=random_string&redirect_uri=http://localhost:3000/auth&duration=permanent&scope=read submit identity`} className={cn(styles.button, className)}>
-      {avatarSrc ? <img className={styles.avatar} src={avatarSrc} alt={userName} /> : <Icons name={EComponents.AvatarIcon} className={cn(styles.img, styles.avatar)} />}
+    <a
+      href={`https://www.reddit.com/api/v1/authorize?client_id=${ID}&response_type=code&state=random_string&redirect_uri=http://localhost:3000/auth&duration=permanent&scope=read submit identity`}
+      className={cn(styles.button, className)}
+    >
+      {avatarSrc ? (
+        <img className={styles.avatar} src={avatarSrc} alt={userName} />
+      ) : (
+        <Icons
+          name={EComponents.AvatarIcon}
+          className={cn(styles.img, styles.avatar)}
+        />
+      )}
 
       <p className={styles.text}>{userName || 'Аноним'}</p>
     </a>
-  )
+  );
 }
