ee8feb744d16b0e715a9af9a5257c37f0c5e01e5
Ivan Limarenko <i.limarenko@brightlab.me>
2022-04-17 22:05:59 +0300

    added formik package

--- src/shared/CommentForm/CommentForm.tsx
+++ src/shared/CommentForm/CommentForm.tsx
@@ -1,35 +1,38 @@
-import React, {ChangeEvent, FormEvent} from 'react';
+import React, {ChangeEvent, FormEvent, forwardRef} from 'react';
 import styles from './styles.scss';
 
 interface CommentFormProps {
   className: string;
-  value: string;
+  values: {comment: string};
+  errors: {comment: string}
   onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
   onBlur: (event: FormEvent) => void;
   onSubmit: () => void;
   isFocus: boolean;
   name: string;
+  touched: {comment: boolean};
   setIsFocus: (arg0: boolean) => void;
-  ref: React.Ref<HTMLTextAreaElement>
 }
 
-const CommentForm: React.FC<CommentFormProps> = ({className, setIsFocus, value, onChange, onBlur, onSubmit, isFocus, name, ref}) => {
+const CommentForm: React.FC<CommentFormProps> = ({className, touched, setIsFocus, values, errors, onChange, onBlur, onSubmit, isFocus, name}) => {
 
   return (
   <form className={`${styles.form} ${className}`} onSubmit={onSubmit}>
 
     <div className={styles.wrapTextarea}>
-      <textarea ref={ref} onFocus={() => setIsFocus(true)} onBlur={onBlur} className={styles.textaria} value={value}  onChange={onChange}/>
+      <textarea name="comment" onFocus={() => setIsFocus(true)} onBlur={onBlur} className={styles.textaria} value={values.comment}  onChange={onChange}/>
 
-      {!isFocus && <p className={styles.placeholder}><span className={styles.userPlaceholder}>{name}</span>, оставьте ваш комментарий</p>}
+      {(!isFocus && !values.comment) && <p className={styles.placeholder}><span className={styles.userPlaceholder}>{name}</span>, оставьте ваш комментарий</p>}
     </div>
+
+    {errors.comment && touched.comment && (<p>{ errors.comment }</p>)}
 
     <div className={styles.containerButtons}>
       <div className={styles.wrapButtons}></div>
-      <button className={styles.button}>Комментировать</button>
+      <button type='submit' className={styles.button}>Комментировать</button>
     </div>
   </form>
   )
-}
+};
 
 export default CommentForm;
--- src/shared/CommentFormContainer/CommentFormContainer.tsx
+++ src/shared/CommentFormContainer/CommentFormContainer.tsx
@@ -1,40 +1,52 @@
-import React, { ChangeEvent, FormEvent, useContext, useRef, useState } from 'react';
-import { userContext } from '@shared/context/userContext';
+import React, { ChangeEvent, FormEvent, useContext, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, updateComment } from '../../store/reducer';
 import { CommentForm } from '../CommentForm';
+import { Formik, FormikErrors, FormikHandlers } from 'formik';
 
 interface ContainerCommentFormProps {
   className: string;
 }
 
-const CommentFormContainer: React.FC<ContainerCommentFormProps> = ({className}) => {
+interface FormValues {
+  comment: string;
+}
+
+const CommentFormContainer: React.FC<ContainerCommentFormProps> = ({ className }) => {
   const value = useSelector<RootState, string>(store => store.commentText);
   const dispatch = useDispatch();
 
   const [isFocus, setIsFocus] = useState<boolean>(false);
-  const refTextaria = useRef(null);
-  const { name } = useContext(userContext);
 
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault();
+  const { name } = useSelector<RootState>(store => store.me.data);
+
+  function handleSubmit(values: FormValues) {
+    console.log('Отправлено: ', values.comment);
   }
 
-  function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    dispatch(updateComment(event.target.value));
+  function updateInputState(ev: ChangeEvent, callback: FormikHandlers['handleChange'], values: FormValues) {
+    callback(ev);
+    dispatch(updateComment(values.comment));
   }
 
-  function handleOnBlur() {
-    const textareaElement: HTMLTextAreaElement | null = refTextaria.current;
+  const initialValues: FormValues = {
+    comment: value || ''
+  }
 
-    if(textareaElement?.value === '') {
-      setIsFocus(false)
+  function validate(values: FormValues) {
+    const errors: FormikErrors<FormValues> = {};
+    if (values.comment.length <= 3) {
+      errors.comment = 'Поле должно содержать более 3-х символов';
     }
+    return errors;
   }
 
   return (
-    <CommentForm value={value} ref={refTextaria} setIsFocus={setIsFocus} isFocus={isFocus} className={className} name={name || ''} onChange={handleChange} onBlur={handleOnBlur} onSubmit={handleSubmit} />
-  )
+    <Formik initialValues={initialValues} validate={validate} onSubmit={handleSubmit} handleChange>
+      {
+        ({ values, errors, handleSubmit, handleChange, handleBlur, touched }) => <CommentForm values={values} touched={touched} errors={errors} setIsFocus={setIsFocus} isFocus={isFocus} className={className} name={name || ''} onChange={(ev) => {(updateInputState(ev, handleChange, values))}} onBlur={handleBlur} onSubmit={handleSubmit} />
+      }
+    </Formik>)
 }
 
 export default CommentFormContainer;
